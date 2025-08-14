25624cd7fea767fb45a83bc55af19975df0ad395
Sasha <lonn.sasha@gmail.com>
2025-08-14 10:47:36 +0200

    led class i c + adding real LED

--- led_class/led_class.c
+++ led_class/led_class.c
@@ -6,6 +6,63 @@ void Led_ctor(Led_Type * const me, LedColor_Type _color,LedState_Type _state){
 
 	me->color = _color;
 	me->state = _state;
+	
+		/*Hardware INIT*/
+	
+	RCC->AHB1ENR |= LED_PORT_CLOCK;
+	
+	switch(_color){
+		
+		case RED:
+			LED_PORT->MODER |= LED_RED_MODE_BIT;
+		if (me->state ==ON){
+				LED_PORT->ODR |=LED_RED_PIN;
+			printf("RED LDE is set on \n\r");
+		} 
+		else{
+		LED_PORT->ODR &= ~LED_RED_PIN;
+			printf("RED LDE is set off \n\r");
+		}
+		break;
+		case GREEN:
+			LED_PORT->MODER |= LED_GREEN_MODE_BIT;
+		if (me->state ==ON){
+				LED_PORT->ODR |=LED_GREEN_PIN;
+			printf("GREEN LDE is set on \n\r");
+		} 
+		else{
+		LED_PORT->ODR &= ~LED_GREEN_PIN;
+			printf("GREEN LDE is set off \n\r");
+		}
+		break;
+		case BLUE:
+			LED_PORT->MODER |= LED_BLUE_MODE_BIT;
+		if (me->state ==ON){
+				LED_PORT->ODR |=LED_BLUE_PIN;
+			printf("BLUE LDE is set on \n\r");
+		} 
+		else{
+		LED_PORT->ODR &= ~LED_BLUE_PIN;
+			printf("BLUE LDE is set off \n\r");
+		}
+		break;
+		
+		case YELLOW:
+			LED_PORT->MODER |= LED_YELLOW_MODE_BIT;
+		if (me->state ==ON){
+				LED_PORT->ODR |=LED_YELLOW_PIN;
+			printf("YELLOW LDE is set on \n\r");
+		} 
+		else{
+		LED_PORT->ODR &= ~LED_YELLOW_PIN;
+			printf("YELLOW LDE is set off \n\r");
+		}
+		break;
+		
+	
+	}
+	
+	
 
 
 }
@@ -13,20 +70,54 @@ void Led_ctor(Led_Type * const me, LedColor_Type _color,LedState_Type _state){
 void Led_setState(Led_Type *const me,LedState_Type _state){
 	
 	me->state = _state;
+
 	switch(me->color){
 		
 		case RED:
-			printf("The RED led is set to %d\n\r",me->state);
-			break;
-		case GREEN:	
-			printf("The GREEN led is set to %d\n\r",me->state);
-			break;
-		case BLUE:	
-			printf("The BLUE led is set to %d\n\r",me->state);
-			break;
-		case YELLOW:	
-			printf("The YELLOW led is set to %d\n\r",me->state);
-			break;
+			LED_PORT->MODER |= LED_RED_MODE_BIT;
+			if (me->state ==ON){
+				LED_PORT->ODR |=LED_RED_PIN;
+				printf("RED LDE is set on \n\r");
+		} 
+			else{
+				LED_PORT->ODR &= ~LED_RED_PIN;
+				printf("RED LDE is set off \n\r");
+		}
+		break;
+		case GREEN:
+			LED_PORT->MODER |= LED_GREEN_MODE_BIT;
+			if (me->state ==ON){
+				LED_PORT->ODR |=LED_GREEN_PIN;
+				printf("GREEN LDE is set on \n\r");
+		} 
+		else{
+		LED_PORT->ODR &= ~LED_GREEN_PIN;
+			printf("GREEN LDE is set off \n\r");
+		}
+		break;
+		case BLUE:
+			LED_PORT->MODER |= LED_BLUE_MODE_BIT;
+		if (me->state ==ON){
+				LED_PORT->ODR |=LED_BLUE_PIN;
+			printf("BLUE LDE is set on \n\r");
+		} 
+		else{
+		LED_PORT->ODR &= ~LED_BLUE_PIN;
+			printf("BLUE LDE is set off \n\r");
+		}
+		break;
+		
+		case YELLOW:
+			LED_PORT->MODER |= LED_YELLOW_MODE_BIT;
+		if (me->state ==ON){
+				LED_PORT->ODR |=LED_YELLOW_PIN;
+			printf("YELLOW LDE is set on \n\r");
+		} 
+		else{
+		LED_PORT->ODR &= ~LED_YELLOW_PIN;
+			printf("YELLOW LDE is set off \n\r");
+		}
+		break;
 		
 		
 	}
--- led_class/led_class.h
+++ led_class/led_class.h
@@ -1,7 +1,26 @@
 #ifndef LED_H
 #define LED_H
 #include <stdint.h>
+#include "stm32f4xx.h"                  // Device header
 #include "uart.h" 
+
+
+
+#define LED_PORT  GPIOB
+
+#define LED_PORT_CLOCK (1u <<1)
+
+#define  LED_RED_PIN (1u<<14)
+#define LED_GREEN_PIN (1u <<12)
+#define LED_BLUE_PIN (1u<<15)
+#define LED_YELLOW_PIN (1u << 13)
+
+#define LED_RED_MODE_BIT (1u<<29)
+#define LED_GREEN_MODE_BIT (1u<<24)
+#define LED_YELLOW_MODE_BIT (1u<<26)
+#define LED_BLUE_MODE_BIT (1u<<30)
+
+
 
 typedef enum{
 
